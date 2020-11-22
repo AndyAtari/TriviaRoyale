af540644b330237fda338e57c149a91bb883f035
Andrew Alteri <andrewsalteri@gmail.com>
2020-11-22 17:55:56 -0600

    DB test and updated schema.

--- TR-backend/db/seeds.rb
+++ TR-backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+movie = Category.create(:name => "Movies")
+
+question_1 = Question.create(:category_id => 1, :difficulty => "medium", 
+    :trivia => "The Xenomorph from the sci-fi film 'Alien' has acidic blood.",
+    :answers => "False", :correct_answer => "True")

280fe4fd8e927aa63963fab7d912a0da6bc207ae
Andrew Alteri <andrewsalteri@gmail.com>
2020-11-25 16:01:15 -0600

    Created a submit button function

--- TR-frontend/src/index.js
+++ TR-frontend/src/index.js
@@ -6,7 +6,6 @@ document.addEventListener("click", function() {
 
 const BACKEND_URL = 'http://localhost:3000';
 const main = document.querySelector("main");
-const triviaCard = document.getElementById("triviaCard")
 const buttonContainer = document.getElementById("button-container");
 
 function fetchTrivia() {
@@ -19,17 +18,31 @@ function fetchTrivia() {
 }
 
 function putsTriviaOnPage(question) {
-    //questions.forEach(question => {
         main.innerHTML += `
         <div class="card" data-id="${question.id}"><p>${question.trivia}</p>
         <p>${question.answer_a}<input type="radio" name="answer" value="${question.answer_a}">
         <p>${question.answer_b}<input type="radio" name="answer" value="${question.answer_b}"></p>
         </div>`
+        createSubmitButton();
     }
 
-buttonContainer.addEventListener("click", function() {
+    fetchTrivia();
+
+buttonContainer.addEventListener("click", function(e) {
+    if (e.target.className === "button1") {
     main.innerHTML = "";
     fetchTrivia();
+    }
 })
+
+function createSubmitButton() {
+        buttonContainer.innerHTML = ""
+        const btn = document.createElement("button");
+        btn.className = "button1";
+        btn.innerText = "Submit?";
+        buttonContainer.appendChild(btn)
+}
+
+
 
 })

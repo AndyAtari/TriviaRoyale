0f0ec0031790cafa485f2aa4ad2910e23f6de9cc
Andrew Alteri <andrewsalteri@gmail.com>
2020-12-04 20:16:24 -0600

    Rendered scoreboard from api data.

--- TR-frontend/src/components/highScore.js
+++ TR-frontend/src/components/highScore.js
@@ -6,12 +6,18 @@ class HighScoreBoard {
     }
     
     static getAll() {
-        api.getAllScores().then((scores) => console.log(scores))
+        api.getAllScores().then((scores) => new HighScoreBoard(scores))
     }
 
     renderScoreBoard() {
         this.scores.forEach(function(score) {
-            
+            const highScoreTable = document.getElementById("high-score")
+            let tr = document.createElement("tr");
+            tr.innerHTML += `
+            <td>${score.user}</td>
+            <td>${score.score}</td>
+            `
+            highScoreTable.appendChild(tr);
         })
 
     }

ec979d2d3f8933b7b650693740a6b1db5c4223ba
Andrew Alteri <andrewsalteri@gmail.com>
2020-12-07 15:15:47 -0600

    Refactoring code to delete unused code.

--- TR-frontend/src/index.js
+++ TR-frontend/src/index.js
@@ -61,110 +61,19 @@ function renderForm() {
 
 
 
-// let randomNum = Math.floor(Math.random()*3)+1;
-
-// function createSubmitButton() {
-//     buttonContainer.innerHTML = ""
-//     const btn = document.createElement("button");
-//     btn.className = "button-submit";
-//     btn.innerText = "Submit?";
-//     buttonContainer.appendChild(btn)
-// }
-
-
-
-// function fetchTrivia(category) {
-
-//     let randomNum = Math.floor(Math.random()*9)+1;
-
-//     fetch(`${BACKEND_URL}/categories/${category}/questions/${randomNum}`)
-//     .then(response => response.json())
-//     .then(putsTriviaOnPage);
-    
-//     }
 
 
 
 
-// function putsTriviaOnPage(question) {
-        // main.innerHTML += `
-        // <div class="card" data-id="${question.id}"><p>${question.trivia}</p>
-        // <p>${question.answer_a}<input type="radio" name="answer"  value="${question.answer_a}">
-        // <p>${question.answer_b}<input type="radio" name="answer"  value="${question.answer_b}"></p>
-        // <input type="hidden" id="correct-answer" name="correct_answer" value="${question.correct_answer}">
-        // </div>`
-    
-//         createSubmitButton();
-//     }
 
 
-// function renderBRGif() {
-//     const img = document.createElement("img");
-//     img.src = "https://media.giphy.com/media/kEdI683LJtrGmTVVCM/giphy.gif"
-//     gifDiv.appendChild(img);
-
-// }
 
 
    
 
-// buttonContainer.addEventListener("click", function(e) {
-
-//     if (e.target.className === "button-submit") {
-//         check();
-//         main.innerHTML = "";
-//     }
-//     if (e.target.className === "button-continue") {
-//         gifDiv.innerHTML = "";
-//         fetchTrivia();
-//     }
-//     if (e.target.className === "button-ng") {
-//         // const catInput = document.querySelector("input:checked").value;
-//         cat.innerHTML = "";
-//         gifDiv.innerHTML = "";
-//         fetchTrivia();
-//     }
-// })
 
 
 
-// function createSubmitButton() {
-//         buttonContainer.innerHTML = ""
-//         const btn = document.createElement("button");
-//         btn.className = "button-submit"; hide
-//         btn.innerText = "Submit?";
-//         buttonContainer.appendChild(btn)
-// }
-
-// function createNextButton() {
-//         buttonContainer.innerHTML = "";
-//         const NextBtn = document.createElement("button");
-//         NextBtn.className = "button-continue";
-//         NextBtn.innerText = "Continue to Extraction?";
-//         buttonContainer.appendChild(NextBtn);
-// }
-
-// function check() {
-//     let checkedValue = document.querySelector("input:checked").value;
-//     let correctAnswer = document.getElementById("correct-answer").value;
-//     if (checkedValue === correctAnswer) {
-//         renderBRGif();
-//         createNextButton();
-//     }
-//     else {
-//         renderGameOverGif();
-//         buttonContainer.innerHTML = "";
-//         setTimeout("location.reload(true);", 5000);
-//     }
-    
-//     }
-
-    
-//     // api.getAllCategories().then(renderCategories);
-//     createNewGameGif();
-//     // CategoryButton.getAll();
-
-// })
 
 
 

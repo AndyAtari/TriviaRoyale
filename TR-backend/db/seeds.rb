6c1b11354efd3027e962d42a6ce2457172115b66
Andrew Alteri <andrewsalteri@gmail.com>
2020-12-07 18:22:43 -0600

    Seeded more questions and styled questions.

--- TR-backend/db/seeds.rb
+++ TR-backend/db/seeds.rb
@@ -20,23 +20,34 @@
 #     :trivia => "'Cube','Cube2: Hypercube', and 'Cube Zero' were directed by the same person.",
 #     :answer_a => "True", :answer_b => "False", :correct_answer => "False")
 
+# Question.create(:category_id => 1,
+#     :trivia => "The film Mad Max: Fury Road features the Dies Irae from which composer's requiem?",
+#     :answer_a => "Mozart", :answer_b => "Verdi", :correct_answer => "Verdi")
+
+#     Question.create(:category_id => 1,
+#         :trivia => "In 'Big Hero Six', what were Baymax's motions modeled after?",
+#         :answer_a => "Baby Penguins", :answer_b => "Baby Monkeys", :correct_answer => "Baby Penguins")
+
+        # Question.create(:category_id => 1,
+        #     :trivia => "Which actress portrayed Dr.Grace Augustine in the James Cameron movie 'Avatar'?",
+        #     :answer_a => "Sigourney Weaver", :answer_b => "Jessica Chastain", :correct_answer => "Sigourney Weaver")
+
+#             Question.create(:category_id => 1,
+#                 :trivia => "Which actors made up the trio in 'The Good, the Bad, and the Ugly'?",
+#                 :answer_a => "Yul Brynner, Steve McQueen, and Charles Bronson", :answer_b => "Clint Eastwood, Eli Wallach, and Lee Van Cleef", :correct_answer => "Clint Eastwood, Eli Wallach, and Lee Van Cleef")
+
+
 Question.create(:category_id => 1,
-    :trivia => "The film Mad Max: Fury Road features the Dies Irae from which composer's requiem?",
-    :answer_a => "Mozart", :answer_b => "Verdi", :correct_answer => "Verdi")
+    :trivia => "What did Alfred Hitchcock use as blood in the film Psycho?",
+    :answer_a => "Chocolate Syrup", :answer_b => "Maple Syrup", :correct_answer => "Chocolate Syrup")
 
     Question.create(:category_id => 1,
-        :trivia => "In 'Big Hero Six', what were Baymax's motions modeled after?",
-        :answer_a => "Baby Penguins", :answer_b => "Baby Monkeys", :correct_answer => "Baby Penguins")
+        :trivia => "What is the name of the supercomputer located in the control room in 'Jurassic Park' (1993)?",
+        :answer_a => "Thinking Machines CM-5", :answer_b => "IBM Blue Gene/Q", :correct_answer => "Thinking Machines CM-5")
 
         Question.create(:category_id => 1,
-            :trivia => "Which actress portrayed Dr.Grace Augustine in the James Cameron movie 'Avatar'?",
-            :answer_a => "Sigourney Weaver", :answer_b => "Jessica Chastain", :correct_answer => "Sigourney Weaver")
-
-            Question.create(:category_id => 1,
-                :trivia => "Which actors made up the trio in 'The Good, the Bad, and the Ugly'?",
-                :answer_a => "Yul Brynner, Steve McQueen, and Charles Bronson", :answer_b => "Clint Eastwood, Eli Wallach, and Lee Van Cleef", :correct_answer => "Clint Eastwood, Eli Wallach, and Lee Van Cleef")
-
-
+            :trivia => "What was the wifi password given to Stephen Strange in Doctor Strange?",
+            :answer_a => "Chakra", :answer_b => "Shambala", :correct_answer => "Shambala")
 
 # Category.create(:name => "Battle Royale")
 
@@ -64,8 +75,25 @@ Question.create(:category_id => 1,
 #                         :trivia => "Hunger Games was written by:",
 #                         :answer_a => "Veronica Roth", :answer_b => "Suzanne Collins", :correct_answer => "Suzanne Collins")
 
-Category.create(:name => "Video Games")
+Question.create(:category_id => 2, 
+    :trivia => "The battle royale inspired movie, 'The Condemned' (2007), starred what former WWF/WWE personality?",
+    :answer_a => "Steve Austin", :answer_b => "Dwayne Johnson", :correct_answer => "Steve Austin")
 
-Question.create(:category_id => 3,
-    :trivia => "How many copies of the notorious E.T. game for the Atari 2600 did Atari end up selling?",
-    :answer_a => "250,000", :answer_b => "1.5 Million", :correct_answer => "1.5 Million")
+    Question.create(:category_id => 2, 
+        :trivia => "Death Race 2000 is about ___ racing.",
+        :answer_a => "motorcycle", :answer_b => "automobile", :correct_answer => "automobile")
+
+        Question.create(:category_id => 2, 
+            :trivia => "PUBG is based on a mod of what popular pc game?",
+            :answer_a => "Arma 3", :answer_b => "Half Life 2", :correct_answer => "Arma 3")
+
+            Question.create(:category_id => 2, 
+                :trivia => "Which novel came first?",
+                :answer_a => "The Hunger Games", :answer_b => "Battle Royale", :correct_answer => "Battle Royale")
+            
+
+# Category.create(:name => "Video Games")
+
+# Question.create(:category_id => 3,
+#     :trivia => "How many copies of the notorious E.T. game for the Atari 2600 did Atari end up selling?",
+#     :answer_a => "250,000", :answer_b => "1.5 Million", :correct_answer => "1.5 Million")

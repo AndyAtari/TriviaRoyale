7f1c742dabdb5b574251cc01ac799965307931eb
Andrew Alteri <andrewsalteri@gmail.com>
2020-12-07 21:55:12 -0600

    Seeded more questions.

--- TR-backend/db/seeds.rb
+++ TR-backend/db/seeds.rb
@@ -37,17 +37,17 @@
 #                 :answer_a => "Yul Brynner, Steve McQueen, and Charles Bronson", :answer_b => "Clint Eastwood, Eli Wallach, and Lee Van Cleef", :correct_answer => "Clint Eastwood, Eli Wallach, and Lee Van Cleef")
 
 
-Question.create(:category_id => 1,
-    :trivia => "What did Alfred Hitchcock use as blood in the film Psycho?",
-    :answer_a => "Chocolate Syrup", :answer_b => "Maple Syrup", :correct_answer => "Chocolate Syrup")
+# Question.create(:category_id => 1,
+#     :trivia => "What did Alfred Hitchcock use as blood in the film Psycho?",
+#     :answer_a => "Chocolate Syrup", :answer_b => "Maple Syrup", :correct_answer => "Chocolate Syrup")
 
-    Question.create(:category_id => 1,
-        :trivia => "What is the name of the supercomputer located in the control room in 'Jurassic Park' (1993)?",
-        :answer_a => "Thinking Machines CM-5", :answer_b => "IBM Blue Gene/Q", :correct_answer => "Thinking Machines CM-5")
+#     Question.create(:category_id => 1,
+#         :trivia => "What is the name of the supercomputer located in the control room in 'Jurassic Park' (1993)?",
+#         :answer_a => "Thinking Machines CM-5", :answer_b => "IBM Blue Gene/Q", :correct_answer => "Thinking Machines CM-5")
 
-        Question.create(:category_id => 1,
-            :trivia => "What was the wifi password given to Stephen Strange in Doctor Strange?",
-            :answer_a => "Chakra", :answer_b => "Shambala", :correct_answer => "Shambala")
+#         Question.create(:category_id => 1,
+#             :trivia => "What was the wifi password given to Stephen Strange in Doctor Strange?",
+#             :answer_a => "Chakra", :answer_b => "Shambala", :correct_answer => "Shambala")
 
 # Category.create(:name => "Battle Royale")
 
@@ -75,21 +75,21 @@ Question.create(:category_id => 1,
 #                         :trivia => "Hunger Games was written by:",
 #                         :answer_a => "Veronica Roth", :answer_b => "Suzanne Collins", :correct_answer => "Suzanne Collins")
 
-Question.create(:category_id => 2, 
-    :trivia => "The battle royale inspired movie, 'The Condemned' (2007), starred what former WWF/WWE personality?",
-    :answer_a => "Steve Austin", :answer_b => "Dwayne Johnson", :correct_answer => "Steve Austin")
+# Question.create(:category_id => 2, 
+#     :trivia => "The battle royale inspired movie, 'The Condemned' (2007), starred what former WWF/WWE personality?",
+#     :answer_a => "Steve Austin", :answer_b => "Dwayne Johnson", :correct_answer => "Steve Austin")
 
-    Question.create(:category_id => 2, 
-        :trivia => "Death Race 2000 is about ___ racing.",
-        :answer_a => "motorcycle", :answer_b => "automobile", :correct_answer => "automobile")
+#     Question.create(:category_id => 2, 
+#         :trivia => "Death Race 2000 is about ___ racing.",
+#         :answer_a => "motorcycle", :answer_b => "automobile", :correct_answer => "automobile")
 
-        Question.create(:category_id => 2, 
-            :trivia => "PUBG is based on a mod of what popular pc game?",
-            :answer_a => "Arma 3", :answer_b => "Half Life 2", :correct_answer => "Arma 3")
+#         Question.create(:category_id => 2, 
+#             :trivia => "PUBG is based on a mod of what popular pc game?",
+#             :answer_a => "Arma 3", :answer_b => "Half Life 2", :correct_answer => "Arma 3")
 
-            Question.create(:category_id => 2, 
-                :trivia => "Which novel came first?",
-                :answer_a => "The Hunger Games", :answer_b => "Battle Royale", :correct_answer => "Battle Royale")
+#             Question.create(:category_id => 2, 
+#                 :trivia => "Which novel came first?",
+#                 :answer_a => "The Hunger Games", :answer_b => "Battle Royale", :correct_answer => "Battle Royale")
             
 
 # Category.create(:name => "Video Games")
@@ -97,3 +97,43 @@ Question.create(:category_id => 2,
 # Question.create(:category_id => 3,
 #     :trivia => "How many copies of the notorious E.T. game for the Atari 2600 did Atari end up selling?",
 #     :answer_a => "250,000", :answer_b => "1.5 Million", :correct_answer => "1.5 Million")
+
+Question.create(:category_id => 3,
+    :trivia => "What was the name of the cancelled sequel of Team Fortress?",
+    :answer_a => "Team Fortress 2: Brotherhood of Arms", :answer_b => "Team Fortress 2: Return to Classic", :correct_answer => "Team Fortress 2: Brotherhood of Arms")
+
+    Question.create(:category_id => 3,
+        :trivia => "The end credits sequence in Grand Theft Auto 5 is over half an hour long?",
+        :answer_a => "True", :answer_b => "False", :correct_answer => "True")
+
+        Question.create(:category_id => 3,
+            :trivia => "Which Kirby game first introduced Copy Abilities?",
+            :answer_a => "Kirby's Adventure", :answer_b => "Kirby's Dream Land", :correct_answer => "Kirby's Adventure")
+
+            Question.create(:category_id => 3,
+                :trivia => "Which Pokemon Type was present in the original games, Red and Blue?",
+                :answer_a => "Fairy", :answer_b => "Ice", :correct_answer => "Ice")
+
+                Question.create(:category_id => 3,
+                    :trivia => "The shotgun appears in every numbered Resident Evil game?",
+                    :answer_a => "True", :answer_b => "False", :correct_answer => "True")
+
+                    Question.create(:category_id => 3,
+                        :trivia => "What happened to Half-Life 2 prior to its release, which resulted in Valve starting over the development of the game?",
+                        :answer_a => "Way too many bugs to be fixed", :answer_b => "The source code got leaked", :correct_answer => "The source code got leaked")
+
+                        Question.create(:category_id => 3,
+                            :trivia => "Which horror movie had a sequel in the form of a video game released in August 20, 2002?",
+                            :answer_a => "Alien", :answer_b => "The Thing", :correct_answer => "The Thing")
+
+                            Question.create(:category_id => 3,
+                                :trivia => "Which of these stages is not playable in 'Super Smash Bros. for Wii'?",
+                                :answer_a => "Fountain of Dreams", :answer_b => "Miiverse", :correct_answer => "Fountain of Dreams")
+
+                                Question.create(:category_id => 3,
+                                    :trivia => "The 64 in the Nintendo-64 console refers to what?",
+                                    :answer_a => "The number of megabytes of RAM", :answer_b => "The bits in the CPU architecture", :correct_answer => "The bits in the CPU architecture")
+
+                                    Question.create(:category_id => 3,
+                                        :trivia => "How long are all the cutscenes from Metal Gear Solid 4 (PS3, 2008) combined?",
+                                        :answer_a => "4 hours", :answer_b => "8 hours", :correct_answer => "8 hours")
